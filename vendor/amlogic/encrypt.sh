123cd067b1aea1dcfa5935e6d85aff3d8f3cbcf9
Da Xue <da.xue@libretech.co>
2022-07-11 22:29:59 -0400

    roc-rk3328-cc support

--- vendor/amlogic/encrypt.sh
+++ vendor/amlogic/encrypt.sh
@@ -7,8 +7,6 @@ LBS_finalizeUBoot(){
 		git clone --depth=1 --single-branch "https://github.com/libre-computer-project/libretech-amlogic-blx.git" "$blx_path"
 	fi
 	
-	LBS_UBOOT_BIN_FINAL_PATH="$LBS_UBOOT_PATH/u-boot-amlogic.bin"
-	
 	$encrypt_path --bl3enc --input "$blx_path"/$BOARD_NAME/bl30_new.bin --output "$blx_path"/$BOARD_NAME/bl30.bin.enc
 	$encrypt_path --bl3enc --input "$blx_path"/$BOARD_NAME/bl31.img --output "$blx_path"/$BOARD_NAME/bl31.img.enc
 	#$encrypt_path --bl3enc --input "$BL31" --output "$blx_path"/$BOARD_NAME/bl31.img.enc
--- vendor/amlogic/gxlimg.sh
+++ vendor/amlogic/gxlimg.sh
@@ -12,15 +12,13 @@ LBS_finalizeUBoot(){
 		git clone --depth=1 --single-branch "https://github.com/libre-computer-project/libretech-amlogic-blx.git" "$blx_path"
 	fi
 	
-	local sd="$LBS_UBOOT_PATH/u-boot-amlogic.bin"
-	
 	$gxlimg_path/gxlimg -t bl2 -s "$blx_path"/$BOARD_NAME/bl2_new.bin "$blx_path"/$BOARD_NAME/bl2.bin.enc
 	$gxlimg_path/gxlimg -t bl3x -c "$blx_path"/$BOARD_NAME/bl30_new.bin "$blx_path"/$BOARD_NAME/bl30.bin.enc
 	#$gxlimg_path/gxlimg -t bl3x -c "$blx_path"/$BOARD_NAME/bl31.img "$blx_path"/$BOARD_NAME/bl31.img.enc
 	$gxlimg_path/gxlimg -t bl3x -c "$BL31" "$blx_path"/$BOARD_NAME/bl31.img.enc
 	$gxlimg_path/gxlimg -t bl3x -c "$LBS_UBOOT_PATH"/u-boot.bin "$LBS_UBOOT_PATH"/u-boot.bin.enc
 	#$gxlimg_path/gxlimg -t fip --bl2 "$blx_path"/$BOARD_NAME/bl2.bin.enc --bl30 "$blx_path"/$BOARD_NAME/bl30.bin.enc --bl31 "$blx_path"/$BOARD_NAME/bl31.img.enc --bl33 "$LBS_UBOOT_PATH"/u-boot.bin.enc "$LBS_UBOOT_PATH"/u-boot-amlogic.bin
-	$gxlimg_path/gxlimg -t fip --bl2 "$blx_path"/$BOARD_NAME/bl2.bin.enc --bl30 "$blx_path"/$BOARD_NAME/bl30.bin.enc --bl31 "$blx_path"/$BOARD_NAME/bl31.img.enc --bl33 "$LBS_UBOOT_PATH"/u-boot.bin.enc "$sd"
-	dd if="$sd" of="$LBS_UBOOT_PATH/u-boot-amlogic.usb.bl2" bs=49152 count=1
-	dd if="$sd" of="$LBS_UBOOT_PATH/u-boot-amlogic.usb.tpl" skip=49152 bs=1
+	$gxlimg_path/gxlimg -t fip --bl2 "$blx_path"/$BOARD_NAME/bl2.bin.enc --bl30 "$blx_path"/$BOARD_NAME/bl30.bin.enc --bl31 "$blx_path"/$BOARD_NAME/bl31.img.enc --bl33 "$LBS_UBOOT_PATH"/u-boot.bin.enc "$LBS_UBOOT_BIN_FINAL_PATH"
+	dd if="$LBS_UBOOT_BIN_FINAL_PATH" of="$LBS_UBOOT_PATH/u-boot-amlogic.usb.bl2" bs=49152 count=1
+	dd if="$LBS_UBOOT_BIN_FINAL_PATH" of="$LBS_UBOOT_PATH/u-boot-amlogic.usb.tpl" skip=49152 bs=1
 }
