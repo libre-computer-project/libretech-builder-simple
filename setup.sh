6c82ff563bc961d881f5336b480876a34320f422
Da Xue <da@libre.computer>
2023-04-08 13:54:35 -0400

    add git to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install build-essential flex bison python3-setuptools swig python3-dev libssl-dev u-boot-tools python3-pyelftools
+sudo apt install build-essential flex bison python3-setuptools swig python3-dev libssl-dev u-boot-tools python3-pyelftools git
