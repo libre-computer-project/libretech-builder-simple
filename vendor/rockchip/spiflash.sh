8173647ecb8cbe8e6abc9f9111a0779b64601487
Da Xue <da.xue@libretech.co>
2023-05-07 03:52:06 -0400

    roc-rk3399-pc: update to 2023.04 and use binman

--- vendor/rockchip/spiflash.sh
+++ vendor/rockchip/spiflash.sh
@@ -1,11 +1,7 @@
 sudo mkfs.vfat ${loop_dev}p1
 local loop_mnt="$(mktemp -d)"
 sudo mount ${loop_dev}p1 "$loop_mnt"
-local tpl=$LBS_UBOOT_PATH/tpl/u-boot-tpl.bin
-local spl=$LBS_UBOOT_PATH/spl/u-boot-spl.bin
-local itb=$LBS_UBOOT_PATH/u-boot.itb
-sudo $LBS_UBOOT_PATH/tools/mkimage -n $SPIFLASH_MKIMAGE_NAME -T rkspi -d "$tpl:$spl" "$loop_mnt/idbloader.img"
-sudo cp "$itb" "$loop_mnt"
+sudo cp $LBS_UBOOT_PATH/u-boot-rockchip-spi.bin "$loop_mnt"
 sudo mkimage -A arm -T script -d "$SPIFLASH_SCRIPT" "$loop_mnt/boot.scr"
 df $loop_mnt
 sudo umount "$loop_mnt"
