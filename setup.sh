a8ce870cc6e13246f389f819f18b257e81490933
Da Xue <da.xue@libretech.co>
2023-05-08 15:57:16 -0400

    redhat support

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-sudo apt install build-essential flex bison python3-setuptools swig python3-dev libssl-dev u-boot-tools python3-pyelftools git
+
+if which apt; then
+	sudo apt install build-essential flex bison python3-setuptools swig python3-dev libssl-dev u-boot-tools python3-pyelftools git
+elif which yum; then
+	sudo yum groupinstall 'Development Tools'
+	sudo yum install python3-setuptools swig python3-devel openssl-devel uboot-tools python3-pyelftools git
+else
+	echo "Package manager not supported. Please install build tools, python3-setuptools, python3-dev, python3-pyelftools, openssl-dev, u-boot-tools, swig, and git."
+	exit 1
+fi
