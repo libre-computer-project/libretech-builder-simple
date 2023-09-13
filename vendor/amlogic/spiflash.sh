854aa4f4f65fc5161ec6fcf7e0c2d32c7c7a7210
Da Xue <da.xue@libretech.co>
2023-09-13 17:50:00 -0400

    vendor: amlogic: spiflash: add sha1sum

--- vendor/amlogic/spiflash.sh
+++ vendor/amlogic/spiflash.sh
@@ -2,6 +2,7 @@ sudo mkfs.vfat ${loop_dev}p1
 local loop_mnt="$(mktemp -d)"
 sudo mount ${loop_dev}p1 "$loop_mnt"
 sudo cp "$LBS_UBOOT_BIN_FINAL_PATH" "$loop_mnt/u-boot.bin"
+sha1sum $LBS_UBOOT_BIN_FINAL_PATH | cut -d " " -f 1 | xxd -r -p | sudo tee "$loop_mnt/u-boot.bin.sha1sum" > /dev/null
 
 sudo mkimage -A arm -T script -d "$LBS_SPIFLASH_SCRIPT" "$loop_mnt/boot.scr"
 df $loop_mnt
