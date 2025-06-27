204e91f1b447565401b024331537a810ef0c9b59
Da Xue <da@libre.computer>
2025-06-26 22:22:34 -0400

    lib/git: add LBS_GIT_REMOTE_OVERRIDE

--- lib/git.sh
+++ lib/git.sh
@@ -15,7 +15,22 @@ LBS_GIT_switchBranch(){
 		fi
 		local branch_exist=$(git -C "$path_tar" branch --list "$branch_tar")
 		if [ -z "$branch_exist" ]; then
-			git -C "$path_tar" fetch --depth=1 "$LBS_GIT_REMOTE_DEFAULT" "$branch_tar"
+			if [ -z "$LBS_GIT_REMOTE_OVERRIDE" ]; then
+				git -C "$path_tar" fetch --depth=1 "$LBS_GIT_REMOTE_DEFAULT" "$branch_tar"
+			else
+				for lbs_git_remote_override in $LBS_GIT_REMOTE_OVERRIDE; do
+					if [ "$lbs_git_remote_override" = "$path_tar" ]; then
+						local git_remote_var="${path_tar//-/}_GIT_REMOTE"
+						declare -n git_remote=${git_remote_var^^}
+						if ! git -C "$path_tar" remote show "$git_remote"; then
+							local git_url_var="${path_tar//-/}_GIT_URL"
+							declare -n git_url=${git_url_var^^}
+							git -C "$path_tar" remote add "$git_remote" "$git_url"
+						fi
+						git -C "$path_tar" fetch --depth=1 "$git_remote" "$branch_tar"
+					fi
+				done
+			fi
 			git -C "$path_tar" checkout -b "$branch_tar" FETCH_HEAD
 		else
 			git -C "$path_tar" checkout "$branch_tar"
