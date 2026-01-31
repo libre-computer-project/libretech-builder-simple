c8a8b26bd53b4ab2aab3bfc105f72444b0848b85
Da Xue <da@libre.computer>
2026-01-30 21:47:36 -0500

    gcc: add aarch64-none-elf

--- lib/gcc.sh
+++ lib/gcc.sh
@@ -13,6 +13,10 @@ LBS_GCC_download(){
 		wget --content-disposition "https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-linux.tar.bz2?revision=bc2c96c0-14b5-4bb4-9f18-bceb4050fee7?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Linux,7-2018-q2-update"
 		tar -xf gcc-arm-none-eabi-7-2018-q2-update-linux.tar.bz2
 	fi
+	if [ ! -d "arm-gnu-toolchain-15.2.rel1-x86_64-aarch64-none-elf" ]; then
+		wget --content-disposition "https://developer.arm.com/-/media/Files/downloads/gnu/15.2.rel1/binrel/arm-gnu-toolchain-15.2.rel1-x86_64-aarch64-none-elf.tar.xz"
+		tar -xf arm-gnu-toolchain-15.2.rel1-x86_64-aarch64-none-elf.tar.xz
+	fi
 	if [ ! -d "gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu" ]; then
 		wget "https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/aarch64-linux-gnu/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu.tar.xz"
 		tar -xf gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu.tar.xz
@@ -36,6 +40,7 @@ LBS_GCC_exportPATH(){
 	cd "$LBS_GCC_PATH"
 	if [ "$LBS_ARCH" = "arm64" ]; then
 		export PATH=$PWD/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf/bin:$PATH
+		export PATH=$PWD/arm-gnu-toolchain-15.2.rel1-x86_64-aarch64-none-elf/bin:$PATH
 		export PATH=$PWD/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin:$PATH
 	fi
 	export PATH=$PWD/gcc-arm-none-eabi-7-2018-q2-update/bin:$PATH
